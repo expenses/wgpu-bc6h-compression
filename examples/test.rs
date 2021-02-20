c90699a629f217ae414ed43b3025fe9b0aa2df19
Ashley Ruglys <ashley.ruglys@gmail.com>
2021-02-20 18:43:54 +0100

    Initial commit, 2d works, need to disable push constants

---
+++ examples/test.rs
@@ -0,0 +1,129 @@
+use wgpu::util::DeviceExt;
+use wgpu_bc6h_compression::{CompressionParams, Compressor2D};
+
+fn main() {
+    let mut args = std::env::args().skip(1);
+    let input_filename = args.next().unwrap();
+    let output_filename = args.next().unwrap();
+
+    let dds = ddsfile::Dds::read(&mut std::fs::File::open(&input_filename).unwrap()).unwrap();
+
+    assert_eq!(
+        dds.get_dxgi_format(),
+        Some(ddsfile::DxgiFormat::R32G32B32A32_Float)
+    );
+    assert_eq!(dds.get_depth(), 1);
+
+    let instance = wgpu::Instance::new(wgpu::BackendBit::PRIMARY);
+
+    let adapter =
+        pollster::block_on(instance.request_adapter(&wgpu::RequestAdapterOptions::default()))
+            .unwrap();
+
+    let (device, queue) = pollster::block_on(adapter.request_device(
+        &wgpu::DeviceDescriptor {
+            label: Some("device"),
+            features: wgpu::Features::PUSH_CONSTANTS,
+            limits: wgpu::Limits {
+                max_push_constant_size: 8,
+                ..Default::default()
+            },
+        },
+        None,
+    ))
+    .unwrap();
+
+    let sampler = device.create_sampler(&wgpu::SamplerDescriptor::default());
+
+    let extent = wgpu::Extent3d {
+        width: dds.get_width(),
+        height: dds.get_height(),
+        depth: 1,
+    };
+
+    let texture_data = dds.get_data(0).unwrap();
+
+    let texture = device.create_texture_with_data(
+        &queue,
+        &wgpu::TextureDescriptor {
+            label: Some("uncompressed texture"),
+            size: extent,
+            mip_level_count: 1,
+            sample_count: 1,
+            dimension: wgpu::TextureDimension::D2,
+            format: wgpu::TextureFormat::Rgba32Float,
+            usage: wgpu::TextureUsage::SAMPLED | wgpu::TextureUsage::COPY_DST,
+        },
+        &texture_data,
+    );
+
+    let compressor_2d = Compressor2D::new(&device);
+
+    let mut command_encoder =
+        device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+
+    let buffer_size = extent.width as u64 * extent.height as u64;
+
+    let target_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+        label: None,
+        size: buffer_size,
+        usage: wgpu::BufferUsage::STORAGE | wgpu::BufferUsage::COPY_SRC,
+        mapped_at_creation: false,
+    });
+
+    compressor_2d.compress_to_buffer(
+        &device,
+        &mut command_encoder,
+        &mut CompressionParams {
+            bind_group_label: None,
+            sampler: &sampler,
+            texture: &texture,
+            extent,
+        },
+        &target_buffer,
+    );
+
+    let mappable_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+        label: None,
+        size: buffer_size,
+        usage: wgpu::BufferUsage::COPY_DST | wgpu::BufferUsage::MAP_READ,
+        mapped_at_creation: false,
+    });
+
+    command_encoder.copy_buffer_to_buffer(&target_buffer, 0, &mappable_buffer, 0, buffer_size);
+
+    queue.submit(Some(command_encoder.finish()));
+
+    let slice = mappable_buffer.slice(..);
+
+    let map_future = slice.map_async(wgpu::MapMode::Read);
+
+    device.poll(wgpu::Maintain::Wait);
+
+    pollster::block_on(map_future).unwrap();
+
+    let bytes = slice.get_mapped_range();
+
+    let mut compressed_dds = ddsfile::Dds::new_dxgi(
+        extent.height,
+        extent.width,
+        None,
+        ddsfile::DxgiFormat::BC6H_UF16,
+        None,
+        None,
+        None,
+        false,
+        ddsfile::D3D10ResourceDimension::Texture2D,
+        ddsfile::AlphaMode::Unknown,
+    )
+    .unwrap();
+
+    compressed_dds
+        .get_mut_data(0)
+        .unwrap()
+        .copy_from_slice(&bytes);
+
+    compressed_dds
+        .write(&mut std::fs::File::create(output_filename).unwrap())
+        .unwrap();
+}
---
+++ src/lib.rs
@@ -0,0 +1,186 @@
+pub struct Compressor2D {
+    pub pipeline: wgpu::ComputePipeline,
+    pub bind_group_layout: wgpu::BindGroupLayout,
+}
+
+impl Compressor2D {
+    pub fn new(device: &wgpu::Device) -> Self {
+        let shader_bytes = wgpu::include_spirv!("../shaders/compiled/2d.comp.spv");
+        let shader = device.create_shader_module(&shader_bytes);
+
+        let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            label: Some("wgpu-bc6h-compression 2d bind group layout"),
+            entries: &[
+                wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStage::COMPUTE,
+                    ty: wgpu::BindingType::Texture {
+                        sample_type: wgpu::TextureSampleType::Float { filterable: false },
+                        view_dimension: wgpu::TextureViewDimension::D2,
+                        multisampled: false,
+                    },
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 1,
+                    visibility: wgpu::ShaderStage::COMPUTE,
+                    ty: wgpu::BindingType::Sampler {
+                        comparison: false,
+                        filtering: false,
+                    },
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 2,
+                    visibility: wgpu::ShaderStage::COMPUTE,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Storage { read_only: false },
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                },
+            ],
+        });
+
+        let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            label: Some("wgpu-bc6h-compression 2d pipeline layout"),
+            bind_group_layouts: &[&bind_group_layout],
+            push_constant_ranges: &[wgpu::PushConstantRange {
+                stages: wgpu::ShaderStage::COMPUTE,
+                range: 0..std::mem::size_of::<[u32; 2]>() as u32,
+            }],
+        });
+
+        let pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+            label: Some("wgpu-bc6h-compression 2d pipeline"),
+            layout: Some(&pipeline_layout),
+            module: &shader,
+            entry_point: "main",
+        });
+
+        Self {
+            bind_group_layout,
+            pipeline,
+        }
+    }
+
+    pub fn compress_to_buffer(
+        &self,
+        device: &wgpu::Device,
+        command_encoder: &mut wgpu::CommandEncoder,
+        params: &CompressionParams,
+        buffer: &wgpu::Buffer,
+    ) {
+        let width_in_blocks = params.extent.width / 4;
+        let height_in_blocks = params.extent.height / 4;
+        debug_assert_eq!(params.extent.width % 4, 0);
+        debug_assert_eq!(params.extent.height % 4, 0);
+        debug_assert_eq!(params.extent.depth, 1);
+
+        let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: params.bind_group_label,
+            layout: &self.bind_group_layout,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: wgpu::BindingResource::TextureView(
+                        &params
+                            .texture
+                            .create_view(&wgpu::TextureViewDescriptor::default()),
+                    ),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: wgpu::BindingResource::Sampler(params.sampler),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 2,
+                    resource: buffer.as_entire_binding(),
+                },
+            ],
+        });
+
+        let mut compute_pass =
+            command_encoder.begin_compute_pass(&wgpu::ComputePassDescriptor::default());
+        compute_pass.set_pipeline(&self.pipeline);
+        compute_pass.set_bind_group(0, &bind_group, &[]);
+        compute_pass
+            .set_push_constants(0, bytemuck::bytes_of(&[width_in_blocks, height_in_blocks]));
+        compute_pass.dispatch(
+            dispatch_count(width_in_blocks, 8),
+            dispatch_count(height_in_blocks, 8),
+            1,
+        );
+    }
+
+    pub fn compress_to_texture(
+        &self,
+        device: &wgpu::Device,
+        command_encoder: &mut wgpu::CommandEncoder,
+        params: &CompressionParams,
+        texture_params: &TextureParams,
+    ) -> wgpu::Texture {
+        let buffer = device.create_buffer(&wgpu::BufferDescriptor {
+            label: None,
+            size: params.extent.width as u64 * params.extent.height as u64,
+            usage: wgpu::BufferUsage::STORAGE | wgpu::BufferUsage::COPY_SRC,
+            mapped_at_creation: false,
+        });
+
+        self.compress_to_buffer(device, command_encoder, params, &buffer);
+
+        let texture = device.create_texture(&wgpu::TextureDescriptor {
+            label: texture_params.label,
+            size: params.extent,
+            mip_level_count: 1,
+            sample_count: 1,
+            dimension: wgpu::TextureDimension::D2,
+            format: wgpu::TextureFormat::Bc6hRgbUfloat,
+            usage: texture_params.usage | wgpu::TextureUsage::COPY_DST,
+        });
+
+        command_encoder.copy_buffer_to_texture(
+            wgpu::BufferCopyView {
+                buffer: &buffer,
+                layout: wgpu::TextureDataLayout {
+                    offset: 0,
+                    // width / 4 (because a block contains 4 pixels horizontally) * 16 (the block size)
+                    // confusing, I know.
+                    bytes_per_row: params.extent.width * 4,
+                    rows_per_image: params.extent.height,
+                },
+            },
+            wgpu::TextureCopyView {
+                texture: &texture,
+                mip_level: 0,
+                origin: wgpu::Origin3d::ZERO,
+            },
+            params.extent,
+        );
+
+        texture
+    }
+}
+
+pub struct CompressionParams<'a> {
+    pub bind_group_label: Option<&'a str>,
+    pub texture: &'a wgpu::Texture,
+    pub sampler: &'a wgpu::Sampler,
+    pub extent: wgpu::Extent3d,
+}
+
+pub struct TextureParams<'a> {
+    pub label: Option<&'a str>,
+    pub usage: wgpu::TextureUsage,
+}
+
+fn dispatch_count(num: u32, group_size: u32) -> u32 {
+    let mut count = num / group_size;
+    let rem = num % group_size;
+    if rem != 0 {
+        count += 1;
+    }
+
+    count
+}
